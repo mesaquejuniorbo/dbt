72f4f7b7623fb70c881b3bc109e02215b87b344d
mesaquejuniorbo <mesaquejunior@gmail.com>
2026-03-13 21:19:23 -0300

    refactor: add test to the correct folder

--- dbt_second_project/tests/generic/test_valid_email.sql
+++ dbt_second_project/tests/generic/test_valid_email.sql
@@ -1,8 +1,13 @@
 {% test valid_email(model, column_name) %}
 
-    select {{ column_name }}
+    with validation as (
+    select {{ column_name }} as email
     from {{ model }}
     where {{ column_name }} is not null
       and {{ column_name }} not like '%_@_%.__%'
+    )
+
+    select *
+    from validation
 
 {% endtest %}
